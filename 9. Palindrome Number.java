e6d8f25fdb734034ae98b9a1f50d894d423e6bd9
Balaji S <sbalaji272006@gmail.com>
2025-10-02 18:57:31 +0530

    29-09-2025

---
+++ 9. Palindrome Number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int temp=x;
+        long y=0;
+        while (temp>0) {
+            y=y*10+temp%10;
+            temp=temp/10;
+        }
+        if(y==x) return true;
+        else return false;
+    }
+}
