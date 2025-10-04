21561ff26ef5c2a055367fcc58dc6c6da21c7c8e
Balaji S <sbalaji272006@gmail.com>
2025-10-04 13:42:30 +0530

    04-10-2025

---
+++ 1822. Sign of the Product of an Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int p=1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0) nums[i]=1;
+            else if(nums[i]<0) nums[i]=-1;
+            else return 0;
+            p*=nums[i];
+        }
+        return p;
+    }
+}
