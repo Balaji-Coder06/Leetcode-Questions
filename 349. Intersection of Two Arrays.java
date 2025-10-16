69ab38cdf5a7600f127189549e304be9f92466f7
Balaji S <sbalaji272006@gmail.com>
2025-10-16 20:43:06 +0530

    16-10-2025

---
+++ 349. Intersection of Two Arrays.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        int temp[] = new int[Math.min(nums1.length, nums2.length)];
+        int k = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    boolean found = false;
+                    for (int m = 0; m < k; m++) {
+                        if (temp[m] == nums1[i]) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        temp[k++] = nums1[i];
+                    }
+                    break;
+                }
+            }
+        }
+        int result[] = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = temp[i];
+        }
+        return result;
+    }
+}
